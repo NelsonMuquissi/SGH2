9ab069805f7fd32a16dcebd8ea3ae454e51d63be
NelsonMuquissi <121813203+NelsonMuquissi@users.noreply.github.com>
2023-06-05 03:51:15 +0100

    commit init

---
+++ Intro.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="imagem/OSZD9396.png" type="image/x-png">
+    <script src="https://kit.fontawesoma.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.js"></script>
+    <link rel="stylesheet" href="intro.css">
+    <script src="arquivo.js">FromBox</script>
+    <title>Front</title>
+</head>
+<body>
+    
+    <header id="her" class="navbar">
+   
+<main>
+    
+        <div class="tete">
+        <a style=" text-decoration: none; height:120px; width:150px; background-color:#fff3; border-radius:9px; color:#333; text-align:center; text-align:center; align-items:center; justify-content:center; display:flex; justify-content:center;" href="TelaProf2/Add.php">Adicionar Professor</a><br>
+        <a style="text-decoration: none; height:120px; width:150px; background-color:#fff3; color:#333; border-radius:9px; margin-left:12px; text-align:center; align-items:center; justify-content:center; display:flex;" href="TelaProf2/view.php">Professores</a><br>
+        <a style="text-decoration: none; height:120px; width:150px; background-color:#fff3; color:#333; border-radius:9px; margin-left:12px; text-align:center; align-items:center; justify-content:center; display:flex;" href="CadastrarTurma09/AddTurma.php">Adicionar Horario de Turmas</a><br>
+        </div>
+        <div class="tete">
+        <a style="color:#333; text-decoration: none; height:120px; width:150px;background-color:#fff3; border-radius:9px; text-align:center; align-items:center; justify-content:center; display:flex;" href="CadastrarTurma09/Turma.php">Horario de Turmas</a><br>
+        <a style=" color:#333; text-decoration: none; height:120px; width:150px; background-color:#fff3; border-radius:9px; margin-left:12px; text-align:center; align-items:center; justify-content:center; display:flex;" href="CadastrarHorario03/AddHorario.php">Adicionar Horario de Professores</a><br>
+        <a style=" color:#333; text-decoration: none; height:120px; width:150px; background-color:#fff3; border-radius:9px;margin-left:12px; text-align:center; align-items:center; justify-content:center; display:flex;" href="CadastrarHorario03/Horario.php">Horario de Professores</a><br>
+        </div>
+        <div class="tete">
+        <a style=" color:#333; text-decoration: none; height:120px; width:150px; background-color:#fff3; border-radius:9px; text-align:center; align-items:center; justify-content:center; display:flex;" href="TurmaProf2/Add.php">Agregar Professores a Turmas</a><br>
+        <a style=" color:#333; text-decoration: none; height:120px; width:150px; background-color:#fff3; border-radius:9px; margin-left:12px; text-align:center; align-items:center; justify-content:center; display:flex;" href="CadastrarHorario03/AddHorario.php">Professores E Turmas</a><br>
+        <a style=" color:#333; text-decoration: none; height:120px; width:150px; background-color:#fff3; border-radius:9px;margin-left:12px;
+         text-align:center; align-items:center; justify-content:center; display:flex;" href="index.html">Sair</a><br>
+         
+        </div>
+</main>
+    </header>
+    
+</body>
+</html>
